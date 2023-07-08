3552d6960212b94e219616605f3267cf066af241
VictoryPashkova <victory.pashkova@gmail.com>
2023-07-08 18:12:43 +0600

    step 3 -f

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 import { program } from 'commander';
+import genDiff from '../src/index.js';
 
 program
   .name('gendiff')
   .argument('<filepath1>')
   .argument('<filepath2>')
-  .description('Compares two configuration files and shows a difference.')
+  .description('Compares two configuration files and shows a differences.')
+  .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2)))
   .option('-V, --version', 'output the version number')
   .helpOption('-h, --help', 'output usage information')
   .option('-f, --format <type>', 'output format');
--- src/index.js
+++ src/index.js
@@ -15,15 +15,24 @@ const getParsedFileData = (filePath) => {
 };
 
 const genDiff = (filepath1, filepath2) => {
-  const fileData1 = { ...getParsedFileData(filepath1) };
-  const fileData2 = { ...getParsedFileData(filepath2) };
+  const fileData1 = { ...getParsedFileData(filepath1.toString()) };
+  const fileData2 = { ...getParsedFileData(filepath2.toString()) };
 
-  const keys = Object.entries(fileData1).concat(Object.entries(fileData2));
+  const keys = _.sortBy(Object.entries(fileData1).concat(Object.entries(fileData2)));
+
   const statuses = keys.map(([key, value]) => {
     if (!_.has(fileData1, key)) {
-      return { key, value, status: 'added' };
+      return {
+        key,
+        value,
+        status: 'added',
+      };
     } if (!_.has(fileData2, key)) {
-      return { key, value, status: 'deleted' };
+      return {
+        key,
+        value,
+        status: 'deleted',
+      };
     } if (fileData1[key] !== fileData2[key]) {
       return {
         key,
@@ -32,21 +41,27 @@ const genDiff = (filepath1, filepath2) => {
         status: 'changed',
       };
     }
-    return { key, value, status: 'unchanged' };
+    return {
+      key,
+      value,
+      status: 'unchanged',
+    };
   });
 
   const makeResultString = statuses.map((node) => {
     if (node.status === 'added') {
-      return `+ ${node.key}: ${node.value}`;
+      return `  + ${node.key}: ${node.value}`;
     } if (node.status === 'deleted') {
-      return `- ${node.key}: ${node.value}`;
+      return `  - ${node.key}: ${node.value}`;
     } if (node.status === 'changed') {
-      return `- ${node.key}: ${node.oldValue} \n + ${node.key}: ${node.newValue}`;
+      return `  - ${node.key}: ${node.oldValue} \n  + ${node.key}: ${node.newValue}`;
     }
-    return `${node.key}: ${node.value}`;
+    return `    ${node.key}: ${node.value}`;
   });
+  const makeUniq = _.uniq(makeResultString);
+  const result = `{\n${makeUniq.join('\n')}\n}`;
 
-  return makeResultString;
+  return result;
 };
 
 export default genDiff;
