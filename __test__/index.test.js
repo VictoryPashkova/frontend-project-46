81a7844383bd62f1f4b04e9d471dc95f3b79a75f
VictoryPashkova <victory.pashkova@gmail.com>
2023-07-12 15:37:46 +0600

    changed file path in index.test.js

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import genDiff from '/Users/v.paskova/Desktop/frontend-project-46/src/index.js';
+import genDiff from '../src/index.js';
 
 const readResultJson = fs.readFileSync(path.resolve(process.cwd(), '__fixtures__/resultjson.txt'),
 'utf-8',
