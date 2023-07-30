09135f8875455d0c8671fe13d2d0091660088694
VictoryPashkova <victory.pashkova@gmail.com>
2023-07-30 16:52:31 +0600

    fixed path5

--- src/parsers.js
+++ src/parsers.js
@@ -1,13 +1,17 @@
 import fs from 'fs';
-import path from 'path';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 import yaml from 'js-yaml';
 
-const getNoramalizedPath = (filepath) => path.resolve('__fixtures__', filepath);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const getFileData = (filepath) => fs.readFileSync(filepath).toString();
 const getFileExtension = (filepath) => path.extname(filepath).toString();
 
 const getParsedFileData = (filepath) => {
-  const fileData = getFileData(getNoramalizedPath(filepath));
+  const fileData = getFileData(getFixturePath(filepath));
   const fileFormat = getFileExtension(filepath);
   if (fileFormat === '.json') {
     return JSON.parse(fileData);
