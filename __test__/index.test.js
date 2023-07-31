d52bbb7d3884a2ac93e84a948a9568de2671a006
VictoryPashkova <victory.pashkova@gmail.com>
2023-07-31 15:19:32 +0600

    changed path.resolve

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,14 +1,10 @@
 /* eslint-disable no-undef */
 /* eslint-disable import/extensions */
 import fs from 'fs';
-import { fileURLToPath } from 'url';
-import path, { dirname } from 'path';
+import path from 'path';
 import genDiff from '../src/index.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const getFixturePath = (filename) => path.join(process.cwd(), '__fixtures__', filename);
+const getFixturePath = (filename) => path.resolve(process.cwd(), '__fixtures__', filename);
 const getResult = (filepath) => fs.readFileSync(getFixturePath(filepath), 'utf8');
 
 test('genDiff', () => {
