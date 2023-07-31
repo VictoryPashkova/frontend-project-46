c0be2cff7d496f7bf03498aaf2559b867e94a5e6
VictoryPashkova <victory.pashkova@gmail.com>
2023-07-31 13:29:02 +0600

    fixed getResult

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -9,13 +9,13 @@ import genDiff from '../src/index.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const getResult = (filepath) => fs.readFileSync(getFixturePath(filepath), 'utf8');
+const getNoramalizedPath = (filepath) => path.resolve(process.cwd(), filepath);
+const getResult = (filepath) => fs.readFileSync(getNoramalizedPath(filepath), 'utf8');
 
 test('genDiff', () => {
-  const stylishResult = 'resultStylish.txt';
-  const plainResult = 'resultPlain.txt';
-  const jsonResult = 'resultJson.txt';
+  const stylishResult = '__fixtures__/resultStylish.txt';
+  const plainResult = '__fixtures__/resultPlain.txt';
+  const jsonResult = '__fixtures__/resultJson.txt';
 
   expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(getResult(stylishResult));
   expect(genDiff('__fixtures__/file1.yaml', '__fixtures__/file2.yaml')).toEqual(getResult(stylishResult));
