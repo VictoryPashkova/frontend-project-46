15db8e6bc44e7cc0a40e7fbdf8e1fa1d864b709e
VictoryPashkova <victory.pashkova@gmail.com>
2023-08-02 13:22:16 +0600

    fixed module parsers.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-console */
+import fs from 'fs';
+import path from 'path';
 import getParsedFileData from './parsers.js';
 import buildTree from './buildTree.js';
 import chooseFormat from './formater/index.js';
 
-const genDiff = (filepath1, filepath2, format = 'stylish') => {
-  const fileData1 = getParsedFileData(filepath1);
-  const fileData2 = getParsedFileData(filepath2);
+const getNoramalizedPath = (filepath) => path.resolve(process.cwd(), filepath);
+const getFileData = (filepath) => fs.readFileSync(filepath, 'utf-8');
+const getFileExtension = (filepath) => path.extname(filepath).toString();
 
-  const compared2Datas = buildTree(fileData1, fileData2);
+const genDiff = (filepath1, filepath2, format = 'stylish') => {
+  const fileData1 = getFileData(getNoramalizedPath(filepath1));
+  const fileData2 = getFileData(getNoramalizedPath(filepath2));
+  const file1Format = getFileExtension(filepath1).slice(1);
+  const file2Format = getFileExtension(filepath2).slice(1);
+  const parsedData1 = getParsedFileData(fileData1, file1Format);
+  const parsedData2 = getParsedFileData(fileData2, file2Format);
+
+  const compared2Datas = buildTree(parsedData1, parsedData2);
   const result = chooseFormat(compared2Datas, format);
   return result;
 };
--- src/parsers.js
+++ src/parsers.js
@@ -1,20 +1,16 @@
-import fs from 'fs';
-import path from 'path';
 import yaml from 'js-yaml';
 
-const getNoramalizedPath = (filepath) => path.resolve(process.cwd(), filepath);
-const getFileData = (filepath) => fs.readFileSync(filepath, 'utf-8');
-const getFileExtension = (filepath) => path.extname(filepath).toString();
-
-const getParsedFileData = (filepath) => {
-  const fileData = getFileData(getNoramalizedPath(filepath));
-  const fileFormat = getFileExtension(filepath);
-  if (fileFormat === '.json') {
-    return JSON.parse(fileData);
-  } if (fileFormat === '.yaml' || fileFormat === '.yml') {
-    return yaml.load(fileData);
+const getParsedFileData = (fileData, fileFormat) => {
+  switch (fileFormat) {
+    case 'json':
+      return JSON.parse(fileData);
+    case 'yaml':
+      return yaml.load(fileData);
+    case 'yml':
+      return yaml.load(fileData);
+    default:
+      throw new Error(`Unknowen format ${fileFormat}`);
   }
-  return `Unknowen format ${fileFormat}`;
 };
 
 export default getParsedFileData;
