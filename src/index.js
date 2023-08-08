ce7eba2eb1e01fc0302bc7035cf78945d63c9e3a
VictoryPashkova <victory.pashkova@gmail.com>
2023-08-08 13:57:29 +0600

    fixed getFileData

--- src/index.js
+++ src/index.js
@@ -5,18 +5,20 @@ import buildTree from './buildTree.js';
 import chooseFormat from './formater/index.js';
 
 const getNoramalizedPath = (filepath) => path.resolve(process.cwd(), filepath);
-const getFileData = (filepath) => fs.readFileSync(filepath, 'utf-8');
+const readFileData = (filepath) => fs.readFileSync(filepath, 'utf-8');
 const getFileExtension = (filepath) => path.extname(filepath).toString();
 
-const genDiff = (filepath1, filepath2, format = 'stylish') => {
-  const fileData1 = getFileData(getNoramalizedPath(filepath1));
-  const fileData2 = getFileData(getNoramalizedPath(filepath2));
-  const file1Format = getFileExtension(filepath1).slice(1);
-  const file2Format = getFileExtension(filepath2).slice(1);
-  const parsedData1 = getParsedFileData(fileData1, file1Format);
-  const parsedData2 = getParsedFileData(fileData2, file2Format);
+const getFileData = (filepath) => {
+  const fileData = readFileData(getNoramalizedPath(filepath));
+  const fileFormat = getFileExtension(filepath).slice(1);
+  return getParsedFileData(fileData, fileFormat);
+};
 
-  const compared2Datas = buildTree(parsedData1, parsedData2);
+const genDiff = (filepath1, filepath2, format = 'stylish') => {
+  const fileData1 = getFileData(filepath1);
+  const fileData2 = getFileData(filepath2);
+
+  const compared2Datas = buildTree(fileData1, fileData2);
   const result = chooseFormat(compared2Datas, format);
   return result;
 };
