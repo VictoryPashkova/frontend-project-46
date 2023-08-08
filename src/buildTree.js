74642738ea845ab3595524862cb5fa13803c9f69
VictoryPashkova <victory.pashkova@gmail.com>
2023-08-08 14:03:51 +0600

    buildTree refactoring

--- src/buildTree.js
+++ src/buildTree.js
@@ -1,29 +1,15 @@
 import _ from 'lodash';
 
 const buildTree = (fileData1, fileData2) => {
-  const keys = _.union(_.keys(fileData1), _.keys(fileData2));
-  const sortedKeys = _.sortBy(keys);
-
-  const comparisonTree = sortedKeys.map((key) => {
+  const keys = _.sortBy(_.union(_.keys(fileData1), _.keys(fileData2)));
+  const comparisonTree = keys.map((key) => {
     if (_.isPlainObject(fileData1[key]) && _.isPlainObject(fileData2[key])) {
-      return {
-        key,
-        children: buildTree(fileData1[key], fileData2[key]),
-        status: 'nested',
-      };
+      return { key, children: buildTree(fileData1[key], fileData2[key]), status: 'nested' };
     }
     if (!_.has(fileData1, key)) {
-      return {
-        key,
-        value: fileData2[key],
-        status: 'added',
-      };
+      return { key, value: fileData2[key], status: 'added' };
     } if (!_.has(fileData2, key)) {
-      return {
-        key,
-        value: fileData1[key],
-        status: 'removed',
-      };
+      return { key, value: fileData1[key], status: 'removed' };
     } if (fileData1[key] !== fileData2[key]) {
       return {
         key,
@@ -32,11 +18,7 @@ const buildTree = (fileData1, fileData2) => {
         status: 'updated',
       };
     }
-    return {
-      key,
-      value: fileData1[key],
-      status: 'unchanged',
-    };
+    return { key, value: fileData1[key], status: 'unchanged' };
   });
   return _.uniq(comparisonTree);
 };
